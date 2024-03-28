575a7f70624a105c026f07bf7b0dcbd9fae1e37b
youabledev <youable.framios@gmail.com>
2024-03-29 00:02:57 +0900

    first commit

---
+++ src/main/java/com/youable/bestudy/BeStudyApplication.java
@@ -0,0 +1,13 @@
+package com.youable.bestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youable/bestudy/BeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youable.bestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
