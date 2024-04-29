7f41e1ae9d1429b3e498ba5959c7400947ef0d1f
youabledev <youable.framios@gmail.com>
2024-04-29 20:59:25 +0900

    Test: Item Service 테스트 코드 추가

--- src/test/java/com/youable/bestudy/service/ItemServiceTest.java
+++ src/test/java/com/youable/bestudy/service/ItemServiceTest.java
@@ -6,9 +6,8 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Transactional
 @SpringBootTest
